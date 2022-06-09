0137c73aa87d9eaf889a97479e044a6c37da2d53
mateushzet <72745420+mateushzet@users.noreply.github.com>
2022-06-09 15:34:28 +0200

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Shop shop = new Shop();
-        while(true) {
-        shop.menu();
-       }
-       //shop.input.close();
-    }
-}
